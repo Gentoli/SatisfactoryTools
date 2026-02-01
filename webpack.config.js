63520acecb117fdf1306c5fe31813e8c44f51101
Gentoli <gentoli@live.com>
2026-02-01 12:22:50 -0500

    node 20, webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const TSLintPlugin = require('tslint-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
@@ -63,4 +64,12 @@ module.exports = {
 			}),
 		],
 	},
+	devServer: {
+		historyApiFallback: {
+			disableDotRule: true,
+		},
+		static: {
+			directory: path.join(__dirname, 'www'),
+		},
+	}
 };
